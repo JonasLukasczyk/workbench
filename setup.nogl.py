2b2573ec51cf778d697407309b9f524da5a82617
David Honegger Rogers <dhr@lanl.gov>
2022-04-01 09:58:46 -0600

    checking nogl setup

---
+++ setup.nogl.py
@@ -0,0 +1,38 @@
+import setuptools
+
+# read the description file 
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'doc/description.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setuptools.setup(
+    name="cinemasci",
+    version="2.0.0",
+    author="David H. Rogers",
+    author_email="dhr@lanl.gov",
+    description="Cinema scientific toolset.",
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url="https://github.com/cinemascience",
+    include_package_data=True,
+    packages=[  "cinemasci",
+                "cinemasci.smoke"
+             ],
+    install_requires=[
+        "setuptools>=60",
+        "numpy",
+        "scipy",
+        "matplotlib",
+        "pytest",
+        "py",
+        "imageio",
+        "ipywidgets",
+        "PyOpenGL_accelerate"
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent",
+    ],
+)

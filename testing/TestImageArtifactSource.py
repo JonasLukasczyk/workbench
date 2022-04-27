e61f126d3c7662387568d4a8296214523df0f9ba
David Honegger Rogers <dhr@lanl.gov>
2022-04-27 12:24:59 -0600

    update

--- testing/TestImageArtifactSource.py
+++ testing/TestImageArtifactSource.py
@@ -19,7 +19,7 @@ class CinemaArtifactSourceTest(unittest.TestCase):
         print("Running test: {}".format(self._testMethodName))
 
     def compare(self, a, b ): 
-        results = compare_images( a, b, 1 )
+        results = compare_images( a, b, 150 )
 
         return (results is None)
 

0b858957bf46ac49cd1e76e3916aff3c742f33c6
David Honegger Rogers <dhr@lanl.gov>
2022-07-27 15:38:16 -0600

    updating comments

--- cinemasci/ImageCanny.py
+++ cinemasci/ImageCanny.py
@@ -2,13 +2,14 @@ from .Core import *
 
 import cv2 
 
+# ImageCanny filter
+# Runs opencv canny edge detection filter, and creates an image of
+# those edges, with transparancy everywhere but the edges
 class ImageCanny(Filter):
 
   def __init__(self):
     super().__init__();
-    self.addInputPort("Conversion", ImageConvertType.GREYSCALE);
     self.addInputPort("Thresholds", [100, 150]); 
-    self.addInputPort("Conversion", ImageConvertType.GREYSCALE);
     self.addInputPort("Images", []);
     self.addOutputPort("Images", []);
 
@@ -16,13 +17,16 @@ class ImageCanny(Filter):
     super().update()
 
     result = []
-
     # iterate over all the images in the input images
     for image in self.inputs.Images.get(): 
+        # convert the input data into a form that cv uses
         cvimage = cv2.cvtColor(image.channel["RGBA"], cv2.COLOR_RGB2BGR)
         thresholds = self.inputs.Thresholds.get()
+        # run the canny algorithm, using this object's thresholds
         canny   = cv2.Canny(cvimage, thresholds[0], thresholds[1]) 
+        # convert to cinema format
         cvFinal = cv2.cvtColor(canny, cv2.COLOR_BGR2RGB)
+        # copy and save output data
         outImage = image.copy()
         outImage.channel['RGBA'] = cvFinal 
         result.append(outImage)
